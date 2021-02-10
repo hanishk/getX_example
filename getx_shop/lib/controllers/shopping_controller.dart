805ff13d33f86ff86c519f20f8c2235b1f4518b5
Hanish <hanishk616@gmail.com>
2021-02-10 14:16:43 +0530

    getcontroller

---
+++ getx_shop/lib/controllers/shopping_controller.dart
@@ -0,0 +1,38 @@
+import 'package:get/get.dart';
+import 'package:getx_shop/models/product.dart';
+
+class ShoppingController extends GetxController {
+  //elements which binds to ui use obserable to update ui whenever data changes
+  var products = List<Product>().obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    fetchProducts();
+  }
+
+  void fetchProducts() async {
+    await Future.delayed(Duration(seconds: 1));
+    var productResult = [
+      Product(
+          id: 1,
+          price: 30,
+          productDescription: 'some description about product',
+          productImage: 'abd',
+          productName: 'FirstProd'),
+      Product(
+          id: 2,
+          price: 40,
+          productDescription: 'some description about product',
+          productImage: 'abd',
+          productName: 'SecProd'),
+      Product(
+          id: 3,
+          price: 49.5,
+          productDescription: 'some description about product',
+          productImage: 'abd',
+          productName: 'ThirdProd'),
+    ];
+    products.assignAll(productResult);
+  }
+}
